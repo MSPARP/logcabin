0766cae72afa09dc88764f03a70a0901c9907dfb
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-02-17 23:00:28 +0000

    Use custom types for username and email address.

--- logcabin/models/__init__.py
+++ logcabin/models/__init__.py
@@ -19,6 +19,8 @@ from sqlalchemy.orm import (
 )
 from zope.sqlalchemy import ZopeTransactionExtension
 
+from logcabin.models.types import URLSegment, EmailAddress
+
 Session = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
@@ -26,14 +28,14 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    username = Column(Unicode(50), nullable=False) # Gotta be alphanumeric
+    username = Column(URLSegment, nullable=False)
     password = Column(String(60), nullable=False) # bcrypt hash
     created = Column(DateTime, nullable=False, default=datetime.datetime.now)
     last_online = Column(DateTime, nullable=False, default=datetime.datetime.now)
     last_ip = Column(INET, nullable=False)
     status = Column(Enum("guest", "active", "banned", name="user_status"), nullable=False, default=u"active")
     unban_date = Column(DateTime)
-    email_address = Column(Unicode(255), nullable=False)
+    email_address = Column(EmailAddress, nullable=False)
     email_verified = Column(Boolean, nullable=False, default=False)
     timezone = Column(Unicode(255), nullable=False, default="UTC")
 
---
+++ logcabin/models/types.py
@@ -0,0 +1,26 @@
+import re
+
+from sqlalchemy.types import TypeDecorator, Unicode
+
+
+class RegexValidatedType(TypeDecorator):
+    def process_bind_param(self, value, dialect):
+        if value and not self.regex.match(value):
+            raise ValueError("value must match the regex {}".format(self.regex.pattern))
+        return value
+
+
+class URLSegment(RegexValidatedType):
+    impl = Unicode(50)
+    regex = re.compile("^[-a-zA-Z0-9_]+$")
+
+
+class EmailAddress(RegexValidatedType):
+    impl = Unicode(255)
+    regex = re.compile("^.+@.+\..+$")
+
+
+class Color(RegexValidatedType): # TODO force lower case
+    impl = Unicode(6)
+    regex = re.compile("^[A-Fa-f0-9]{6}$")
+
