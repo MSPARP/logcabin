6340a93044ba41e26687f8462b9956fdab421512
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-02-20 23:56:11 +0000

    Check constraints for email and password.

--- logcabin/models/__init__.py
+++ logcabin/models/__init__.py
@@ -2,6 +2,7 @@ import datetime
 
 from sqlalchemy import (
     func,
+    CheckConstraint,
     Column,
     Index,
     Boolean,
@@ -27,16 +28,24 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+    __table_args__ = (
+        CheckConstraint("""
+            (password IS NOT NULL AND email_address IS NOT NULL AND email_verified IS NOT NULL)
+            or
+            (password IS NULL AND email_address IS NULL AND email_verified IS NULL)
+        """, name="user_password_and_email"),
+        CheckConstraint("unban_date IS NOT NULL OR status = 'banned'", name="user_unban_date"),
+    )
     id = Column(Integer, primary_key=True)
     username = Column(URLSegment, nullable=False)
-    password = Column(String(60), nullable=False) # bcrypt hash
+    password = Column(String(60)) # bcrypt hash
     created = Column(DateTime, nullable=False, default=datetime.datetime.now)
     last_online = Column(DateTime, nullable=False, default=datetime.datetime.now)
     last_ip = Column(INET, nullable=False)
     status = Column(Enum("guest", "active", "banned", name="user_status"), nullable=False, default=u"active")
     unban_date = Column(DateTime)
-    email_address = Column(EmailAddress, nullable=False)
-    email_verified = Column(Boolean, nullable=False, default=False)
+    email_address = Column(EmailAddress)
+    email_verified = Column(Boolean, default=False)
     timezone = Column(Unicode(255), nullable=False, default="UTC")
 
     def __repr__(self):
