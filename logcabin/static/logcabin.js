2930fc49bc5b87a8b46ddc46300d3f9752bd06dc
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-03-01 23:20:57 +0000

    Only allow one submission at a time, and handle network errors.

--- logcabin/static/logcabin.js
+++ logcabin/static/logcabin.js
@@ -5,17 +5,21 @@
         standard_headers.append("X-Requested-With", "XMLHttpRequest");
         for (var form of document.querySelectorAll(".ajax_form")) {
             form.addEventListener("submit", function(e) {
-                /* TODO don't allow re-submitting until complete */
-                /* TODO handle network errors */
+                e.preventDefault();
+                e.stopPropagation();
+                if (this.classList.contains("in_progress")) { return false; }
+                this.classList.add("in_progress");
                 var error_container = this.querySelector(".error");
                 if (error_container) { error_container.textContent = ""; }
                 var data = {};
+                var form = this;
                 fetch(this.action, {
                     method: this.method,
                     headers: standard_headers,
                     credentials: "include",
                     body: new FormData(this),
                 }).then(function(response) {
+                    form.classList.remove("in_progress");
                     return response.json();
                 }).then(function(data) {
                     if (data.error && error_container) {
@@ -23,9 +27,12 @@
                     } else if (data.go_to_url) {
                         location.href = data.go_to_url;
                     }
+                }).catch(function(reason) {
+                    form.classList.remove("in_progress");
+                    if (error_container) {
+                        error_container.textContent = "This request couldn't be completed. Please try again later.";
+                    }
                 });
-                e.preventDefault();
-                e.stopPropagation();
             });
         }
     }
