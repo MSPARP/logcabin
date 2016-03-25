a0bd14922eca21d2ff583c493f7240155bb00ac0
Olly Parton <mysticdragonfly@hotmail.co.uk>
2016-03-25 23:16:12 +0000

    Tables for log subscriptions and favourites.

--- logcabin/models/__init__.py
+++ logcabin/models/__init__.py
@@ -17,6 +17,7 @@ from sqlalchemy import (
 from sqlalchemy.dialects.postgresql import INET
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
+    relationship,
     scoped_session,
     sessionmaker,
 )
@@ -85,6 +86,8 @@ class Log(Base):
     created = Column(DateTime, nullable=False, default=datetime.datetime.now)
     last_modified = Column(DateTime, nullable=False, default=datetime.datetime.now)
 
+Log.creator = relationship(User, backref="logs_created")
+
 
 class Chapter(Base):
     __tablename__ = "chapters"
@@ -95,4 +98,27 @@ class Chapter(Base):
     creator = Column(Integer, ForeignKey(User.id), nullable=False)
     created = Column(DateTime, nullable=False, default=datetime.datetime.now)
     last_modified = Column(DateTime, nullable=False, default=datetime.datetime.now)
+
+Chapter.Log = relationship(Log, backref="chapters")
+Chapter.creator = relationship(User, backref="chapters_created")
+
+
+class LogSubscription(Base):
+    __tablename__ = "log_subscriptions"
+    user_id = Column(Integer, ForeignKey(User.id), primary_key=True)
+    log_id = Column(Integer, ForeignKey(Log.id), primary_key=True)
+    created = Column(DateTime, nullable=False, default=datetime.datetime.now)
+
+LogSubscription.user = relationship(User, backref="log_subscriptions")
+LogSubscription.log = relationship(Log, backref="subscribers")
+
+
+class Favourite(Base):
+    __tablename__ = "favourites"
+    user_id = Column(Integer, ForeignKey(User.id), primary_key=True)
+    log_id = Column(Integer, ForeignKey(Log.id), primary_key=True)
+    created = Column(DateTime, nullable=False, default=datetime.datetime.now)
+
+Favourite.user = relationship(User, backref="favourites")
+Favourite.log = relationship(Log, backref="favourites")
 
